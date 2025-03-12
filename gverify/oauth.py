9d8d551f58928cdb396fd3a8c67a6734097e0e3a
lyle <lyleokoth.mlengineer@gmail.com>
2025-03-12 15:10:41 +0300

    fix: lints the files.

--- gverify/oauth.py
+++ gverify/oauth.py
@@ -15,22 +15,22 @@ class GoogleOAuth(BaseModel):
     api_service_name: str
     api_version: str
     credentials_dir: str
-    credentials_file_name: Optional[str] = 'credentials.json'
+    credentials_file_name: Optional[str] = "credentials.json"
 
     def credentials_to_dict(self, credentials: Credentials) -> dict:
         """Convert credentials to a dict."""
         return {
-            'token': credentials.token,
-            'refresh_token': credentials.refresh_token,
-            'token_uri': credentials.token_uri,
-            'client_id': credentials.client_id,
-            'client_secret': credentials.client_secret,
-            'scopes': credentials.scopes,
+            "token": credentials.token,
+            "refresh_token": credentials.refresh_token,
+            "token_uri": credentials.token_uri,
+            "client_id": credentials.client_id,
+            "client_secret": credentials.client_secret,
+            "scopes": credentials.scopes,
         }
 
     def create_default_credentials_path(self) -> str:
         """Create the default credentials directory."""
-        current_user_home_dir = path.expanduser('~')
+        current_user_home_dir = path.expanduser("~")
         if not path.exists(path.join(current_user_home_dir, self.credentials_dir)):
             mkdir(path.join(current_user_home_dir, self.credentials_dir))
         return path.join(current_user_home_dir, self.credentials_dir)
@@ -46,7 +46,7 @@ class GoogleOAuth(BaseModel):
         credentials: Credentials = None
         credentials_path: str = self.get_default_credentials_path()
         try:
-            with open(credentials_path, 'r', encoding='utf-8') as creds:
+            with open(credentials_path, "r", encoding="utf-8") as creds:
                 credentials = Credentials(**load(creds))
         except FileNotFoundError:
             pass
@@ -61,11 +61,11 @@ class GoogleOAuth(BaseModel):
         flow = Flow.from_client_secrets_file(
             self.secrets_file,
             scopes=self.scopes,
-            redirect_uri='urn:ietf:wg:oauth:2.0:oob',
+            redirect_uri="urn:ietf:wg:oauth:2.0:oob",
         )
-        auth_url, _ = flow.authorization_url(prompt='consent')
-        print('Please go to this URL: {}'.format(auth_url))
-        code = input('Enter the authorization code: ')
+        auth_url, _ = flow.authorization_url(prompt="consent")
+        print("Please go to this URL: {}".format(auth_url))
+        code = input("Enter the authorization code: ")
         flow.fetch_token(code=code)
         credentials: Credentials = flow.credentials
         return credentials
@@ -73,12 +73,12 @@ class GoogleOAuth(BaseModel):
     def save_credentials(self, credentials: Credentials) -> None:
         credentials_dict = self.credentials_to_dict(credentials)
         credentials_path: str = self.get_default_credentials_path()
-        with open(credentials_path, 'w', encoding='utf-8') as f:
+        with open(credentials_path, "w", encoding="utf-8") as f:
             dump(credentials_dict, f)
 
     def credentials_expired(self, credentials: Credentials) -> bool:
         youtube_client = self.get_youtube_client(credentials=credentials)
-        youtube_find_request = youtube_client.search().list(q='', part='id')
+        youtube_find_request = youtube_client.search().list(q="", part="id")
         try:
             youtube_find_request.execute()
         except RefreshError:
